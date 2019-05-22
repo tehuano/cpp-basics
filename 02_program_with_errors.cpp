f7e2d213d464df80f3637a1cf0127d8ad3cdfdc0
Rommel García Hernández <rommelgh@gmail.com>
2019-05-22 15:26:32 -0500

    Add files via upload

---
+++ 02_program_with_errors.cpp
@@ -0,0 +1,11 @@
+*/ Now you should not forget your glasses //
+#include <stream>
+int main
+{
+cout << "If this text",
+cout >> " appears on your display, ";
+cout << " endl;"
+cout << 'you can pat yourself on '
+<< " the back!" << endl.
+return 0;
+)
